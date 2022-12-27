6a07d5ca36bd6e7986fa8710b144d8112d706832
Danyil <danyildiabin@ukr.net>
2022-12-27 17:39:09 +0200

    Code cleanup  - Moved typescript types to separate file  - Added src directory  - rename index.ts to main.ts  - Converted project to use node-next modules  - Settings.json refactor

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,35 @@
-// Dependencies
-// node.js packages
-import * as fs from "fs";
 import { existsSync, writeFileSync, readFileSync, unlinkSync } from "node:fs";
-const { spawnSync } = require("node:child_process");
-// networking
+import { spawnSync } from "node:child_process";
 import { Client, GatewayIntentBits, Message } from "discord.js";
-// backends
-import * as musicaldown from "./backends/musicaldown";
-let backends = new Array<{ (data: string): Promise<string> }>();
-backends.push(musicaldown.getVideoURL);
+import { VideoData, Settings } from "./types.js";
+import { getVideoURL } from "./backends/musicaldown.js";
+import { createInterface } from "readline";
 
-// Driver code
 const bot = new Client({
-    intents: [GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent | GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.GuildMessages |
+        GatewayIntentBits.MessageContent |
+        GatewayIntentBits.Guilds
+    ]
 });
-// TODO Add json validator
-type Settings = {
-    token: string;
-    enable_compression: boolean;
-    codec_to_use: string,
-    embeded_mode: boolean;
-    ffmpeg_path: string;
-    ffprobe_path: string;
-    gallery_dl_path: string;
-};
-let settings: Settings;
-if (!existsSync("./settings.json")) {
-    settings = configurationWizard();
-    const data = JSON.stringify(settings);
-    const encoder = new TextEncoder();
-    writeFileSync("settings.json", encoder.encode(data));
-    alert("Config file saved.");
-} else {
-    const decoder = new TextDecoder("utf-8");
-    const data = readFileSync("./settings.json");
-    settings = JSON.parse(decoder.decode(data));
-}
+
+const settings = getSettings();
 bot.login(settings.token);
 
-// Event handlers
 bot.on("messageCreate", async (message) => {
-    if (message.content == undefined) return;
+    if (message.content == "") return;
     if (message.author.id == bot.user?.id) return;
     const tiktok_links = extractTiktokURLs(message.content);
     const instagram_links = extractInstagramURLs(message.content);
+    if (tiktok_links == undefined && instagram_links == undefined) return;
+
+    let start_time = Date.now();
 
     // Remove embeds from original message for cleaner look
-    if (tiktok_links != undefined || instagram_links != undefined) {
-        try {
-            await message.suppressEmbeds(true);
-        } catch {
-            console.log("bot has no permission to remove embeds, silently skipping...");
-        }
+    try {
+        await message.suppressEmbeds(true);
+    } catch {
+        console.warn("No permission to remove embeds, skipping...");
     }
 
     // Process any tiktok links
@@ -59,7 +37,7 @@ bot.on("messageCreate", async (message) => {
         console.log(`Recieved ${tiktok_links.length} tiktok links!`);
         for (let i = 0; i < tiktok_links.length; i++) {
             const video_data_promise = extractTiktokData(tiktok_links[i], 5);
-            processVideoRequst(video_data_promise, message);
+            processVideoRequst(video_data_promise, message, start_time);
         }
     }
 
@@ -67,37 +45,31 @@ bot.on("messageCreate", async (message) => {
     if (instagram_links != undefined) {
         console.log(`Recieved ${instagram_links.length} instagram links!`);
         for (let i = 0; i < instagram_links.length; i++) {
-            const video_data_promise = extractInstagramData(instagram_links[0], 3);
-            processVideoRequst(video_data_promise, message);
+            const video_data_promise = extractInstagramData(instagram_links[i], 3);
+            processVideoRequst(video_data_promise, message, start_time);
         }
     }
 });
 
-bot.on("ready", async () => {
-    console.log(`Logged in as ${bot.user?.tag}!`);
-});
+bot.on("ready", () => console.log(`Logged in as ${bot.user?.tag}!`));
 
 // Do not crash on unhandled errors
-process.on("unhandledRejection", (error) => {
-    console.error("Unhandled promise rejection:", error);
-});
+process.on("unhandledRejection", (error) => console.error("Unhandled promise rejection:", error));
 
 // Graceful shutdown
-process.on("SIGINT", function () {
+process.on("SIGINT", () => {
     bot.destroy();
     process.exit(0);
 });
 
 // Hack to use graceful shutdown on windows hosts
 if (process.platform === "win32") {
-    let rl = require("readline").createInterface({
+    const rl = createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    rl.on("SIGINT", function () {
-        process.emit("SIGINT");
-    });
+    rl.on("SIGINT", () => process.emit("SIGINT"));
 }
 
 function extractTiktokURLs(message_content: string) {
@@ -143,7 +115,7 @@ async function extractTiktokData(tiktok_url: string, max_tries: number) {
     while (true) {
         tries++;
         try {
-            const video_url = await musicaldown.getVideoURL(tiktok_url);
+            const video_url = await getVideoURL(tiktok_url);
             const video_size = await validateAndGetContentLength(video_url);
             return { url: video_url, size: video_size };
         } catch (error: any) {
@@ -208,7 +180,7 @@ class Status {
     }
 }
 
-function configurationWizard(): Settings {
+function guideAndCreateSettings() {
     const header = `settings.json is missing... Starting configuration wizard.
  _____  _  _    ___       _    _      
 |_   _|(_)| |__| _ ) ___ | |_ (_)__ __
@@ -236,7 +208,7 @@ function configurationWizard(): Settings {
     console.log(header);
     const gallery_dl_path = prompt("Please write where gallery-dl executable is located (skip if it's in PATH):", "gallery-dl") || "";
 
-    return {
+    const data = {
         token: token,
         enable_compression: enable_compression,
         codec_to_use: "h264",
@@ -245,16 +217,24 @@ function configurationWizard(): Settings {
         ffprobe_path: ffprobe_path,
         gallery_dl_path: gallery_dl_path
     };
+    const string_data = JSON.stringify(data);
+    writeFileSync("settings.json", string_data);
+    alert("Config file saved.");
 }
 
-type VideoData = {
-    url: string;
-    size: number;
-};
+// TODO Add json validator
+function getSettings() {
+    if (!existsSync("./settings.json")) {
+        guideAndCreateSettings();
+    }
+    const data = readFileSync("./settings.json", { encoding: "utf-8" });
+    return JSON.parse(data);
+}
 
-async function processVideoRequst(video_data_promise: Promise<VideoData>, reply_to: Message) {
+async function processVideoRequst(video_data_promise: Promise<VideoData>, reply_to: Message, start_time: number) {
     try {
         const video_data = await video_data_promise;
+        console.log(`Video data extraction took ${start_time - Date.now()}ms`);
 
         if (settings.embeded_mode) {
             reply_to.reply({ content: video_data.url, allowedMentions: { repliedUser: false } });
