62df23b9b421ddea76c31bd539bb8bedd1d92d59
Danyil Diabin <danyildiabin@protonmail.com>
2023-02-26 18:03:42 +0200

    Bug fixes and improvements Fixed broken discord.js types Fixed configuration wizzard

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { spawnSync } from "node:child_process";
 import { Client, GatewayIntentBits, Message } from "discord.js";
 import { VideoData, Settings } from "./types.js";
 import { getVideoURL } from "./backends/musicaldown.js";
-import { createInterface } from "readline";
+import * as readline from 'node:readline/promises';
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 const bot = new Client({
     intents: [
@@ -15,8 +17,12 @@ const bot = new Client({
     ]
 });
 
-const settings = getSettings();
-bot.login(settings.token);
+let settings: Settings | undefined = undefined;
+
+getSettings().then((config: Settings)=>{
+    settings = config;
+    bot.login(settings.token);
+});
 
 bot.on("messageCreate", async (message) => {
     if (message.content == "") return;
@@ -38,7 +44,7 @@ bot.on("messageCreate", async (message) => {
     const typing_interval = setInterval(async ()=>{
       await message.channel.sendTyping();
     }, 9000);
-  
+
     const videos_to_process = [];
 
     // Process any tiktok links
@@ -77,7 +83,6 @@ process.on("SIGINT", () => {
 
 // Hack to use graceful shutdown on windows hosts
 if (process.platform === "win32") {
-    const rl = createInterface({ input: process.stdin, output: process.stdout });
     rl.on("SIGINT", () => process.emit("SIGINT"));
 }
 
@@ -87,7 +92,7 @@ async function extractInstagramData(instagram_url: string, max_tries: number) {
     while (true) {
         tries++;
         try {
-            const gallety_dl = spawnSyncWrapper(`${settings.gallery_dl_path} --get-urls --cookies cookies.txt ${instagram_url}`);
+            const gallety_dl = spawnSyncWrapper(`${settings!.gallery_dl_path} --get-urls --cookies cookies.txt ${instagram_url}`);
             if (gallety_dl.status != 0) throw new Error("extracting raw video failed: gallery-dl error, check cookies");
             const links = new TextDecoder("utf-8").decode(gallety_dl.stdout).split("\n");
             if (links == undefined) throw new Error("gallery-dl couldn't find any links");
@@ -131,52 +136,55 @@ async function validateAndGetContentLength(url: string): Promise<number> {
     return parseInt(content_length);
 }
 
-function guideAndCreateSettings() {
-    const header = `settings.json is missing... Starting configuration wizard.
- _____  _  _    ___       _    _      
+async function queryInput(query: string) {
+    const header = ` _____  _  _    ___       _    _
 |_   _|(_)| |__| _ ) ___ | |_ (_)__ __
   | |  | || / /| _ \\/ _ \\|  _|| |\\ \\ /
   |_|  |_||_\\_\\|___/\\___/ \\__||_|/_\\_\\\n`;
     console.clear();
     console.log(header);
-    alert("continue");
-    console.clear();
-    console.log(header);
-    const token = prompt("Please paste your Discord bot token:") || "";
-    console.clear();
-    console.log(header);
-    const enable_compression = confirm("Enable compression for videos bigger than 8MB?\nYou must have ffmpeg and ffprobe in your PATH.\nWithout compression videos will be sent as embbeded link");
-    console.clear();
-    console.log(header);
-    const embeded_mode = confirm("Send all videos as embeded links?\nThose links may look ugly in chat.\nUse this only feature if your internet is super-slow");
-    console.clear();
-    console.log(header);
-    const ffmpeg_path = prompt("Please write where ffmpeg executable is located (skip if it's in PATH):", "ffmpeg") || "";
-    console.clear();
-    console.log(header);
-    const ffprobe_path = prompt("Please write where ffprobe executable is located (skip if it's in PATH):", "ffprobe") || "";
-    console.clear();
-    console.log(header);
-    const gallery_dl_path = prompt("Please write where gallery-dl executable is located (skip if it's in PATH):", "gallery-dl") || "";
+    let result = await rl.question(query);
+    return result.trim();
+}
 
-    const data = {
-        token: token,
-        enable_compression: enable_compression,
+async function confirmSetting(query: string) {
+    while (true) {
+        const response = await queryInput(query + "\nWrite \"Y\" to enable, or \"N\" to disable: ")
+        if (response == "Y") return true;
+        if (response == "N") return false;
+    }
+}
+
+async function guideAndCreateSettings() {
+    let config: Settings = {
+        token: "",
+        embeded_mode: false,
+        gallery_dl_path: "gallery-dl",
+        enable_compression: false,
+        ffmpeg_path: "ffmpeg",
+        ffprobe_path: "ffprobe",
         codec_to_use: "h264",
-        embeded_mode: embeded_mode,
-        ffmpeg_path: ffmpeg_path,
-        ffprobe_path: ffprobe_path,
-        gallery_dl_path: gallery_dl_path
     };
-    const string_data = JSON.stringify(data);
+    await queryInput("settings.json is missing... Starting configuration wizard.\n[Press Enter to continue]");
+    config.token = await queryInput("Please paste your Discord bot token: ");
+    config.embeded_mode = await confirmSetting("Send all videos as embeded links?\nThose links may look ugly in chat.\nDo not use this unless your internet is super-slow");
+    config.gallery_dl_path = (await queryInput("Please write where gallery-dl executable is located (or press Enter if it is already in PATH): ")) || "gallery-dl";
+    config.enable_compression = await confirmSetting("Enable compression for videos bigger than 8MB?\nYou must have ffmpeg and ffprobe in your installed.\nWithout compression big videos will be sent as embbeded link");
+    if (config.enable_compression) {
+        config.codec_to_use = (await queryInput("Specify codec you want to use when compressing video\nfor example \"omx_h264\" if you're on raspberrypi (or press Enter to use h264)")) || "h264";
+        config.ffmpeg_path = (await queryInput("Please write where ffmpeg executable is located (or press Enter if it is already in PATH): ")) || "ffmpeg";
+        config.ffprobe_path = (await queryInput("Please write where ffprobe executable is located (or press Enter if it is already in PATH): ")) || "ffprobe";
+    }
+    const string_data = JSON.stringify(config);
+    console.log(string_data);
     writeFileSync("settings.json", string_data);
-    alert("Config file saved.");
+    console.log("Config file saved.");
 }
 
 // TODO Add json validator
-function getSettings() {
+async function getSettings() {
     if (!existsSync("./settings.json")) {
-        guideAndCreateSettings();
+        await guideAndCreateSettings();
     }
     const data = readFileSync("./settings.json", { encoding: "utf-8" });
     const settings: Settings = JSON.parse(data);
@@ -189,7 +197,8 @@ async function processVideoRequst(video_data_promise: Promise<VideoData>, reply_
         const video_data = await video_data_promise;
         console.debug(`profiling: video extraction ${Date.now() - start_time}ms`);
 
-        if (settings.embeded_mode || video_data.size >= 8_388_608 && !settings.enable_compression) {
+        // Possible undefined here if code changes!
+        if (settings!.embeded_mode || video_data.size >= 8_388_608 && !settings!.enable_compression) {
             await reply_to.reply({ content: video_data.url, allowedMentions: { repliedUser: false } });
             return;
         }
@@ -215,7 +224,8 @@ async function processVideoRequst(video_data_promise: Promise<VideoData>, reply_
         console.debug(`profiling: saved video to disk ${Date.now() - start_time}ms`);
 
         start_time = Date.now();
-        const ffprobe = spawnSyncWrapper(`${settings.ffprobe_path} -v quiet -print_format json -show_streams original.mp4`);
+        // Possible undefined here if code changes!
+        const ffprobe = spawnSyncWrapper(`${settings!.ffprobe_path} -v quiet -print_format json -show_streams original.mp4`);
         if (ffprobe.status != 0) throw new Error("Compression failed: ffprobe error");
         console.debug(`profiling: ffprobe ${Date.now() - start_time}ms`);
 
@@ -226,7 +236,8 @@ async function processVideoRequst(video_data_promise: Promise<VideoData>, reply_
         const video_bitrate = Math.floor((67_108_864 - duration * audio_bitrate - 16_777_216) / duration);
 
         start_time = Date.now();
-        const ffmpeg = spawnSyncWrapper(`${settings.ffmpeg_path} -i original.mp4 -y -b:v ${video_bitrate.toString()} -vcodec ${settings.codec_to_use} compressed.mp4`);
+        // Possible undefined here if code changes!
+        const ffmpeg = spawnSyncWrapper(`${settings!.ffmpeg_path} -i original.mp4 -y -b:v ${video_bitrate.toString()} -vcodec ${settings!.codec_to_use} compressed.mp4`);
         if (ffmpeg.status != 0) throw new Error("Compression failed: ffmpeg error");
         console.debug(`profiling: ffmpeg ${Date.now() - start_time}ms`);
 
